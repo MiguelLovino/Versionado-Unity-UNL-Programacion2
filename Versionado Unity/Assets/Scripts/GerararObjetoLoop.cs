685f012237f2462e988951a5f099ab14b3f555ee
MiguelLovino <miguel_lovino@hotmail.com>
2023-09-29 15:08:39 -0300

    Update GerararObjetoLoop.cs
    Se cambia el comportamiento para que solo genere objetos cuando la camara de la escena lo puede visualizar.

--- Versionado Unity/Assets/Scripts/GerararObjetoLoop.cs
+++ Versionado Unity/Assets/Scripts/GerararObjetoLoop.cs
@@ -17,9 +17,23 @@ public class GerararObjetoLoop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //metodo que se encarga de generar nuestro objeto en loop.
-        InvokeRepeating(nameof(GenerarObjeto), tiempoEspera, tiempoIntervalo);
        
+    }
+
+    //cuando esta en la vista del jugador, se invoca
+    private void OnBecameVisible()
+    {
+        //metodo que se encarga de generar nuestro objeto en loop.
+        Debug.Log("El sprite es visible por la camara, se generan objeto");
+        InvokeRepeating(nameof(GenerarObjeto), tiempoEspera, tiempoIntervalo);
+        
+    }
+
+    //si sale de la vista, para la invocasion
+    private void OnBecameInvisible() 
+    {
+        Debug.Log("El sprite es Invisible por la camara, se detiene la generacion de objetos");
+        CancelInvoke(nameof(GenerarObjeto));
     }
 
     //metodo encargado de instanciar al objeto a invocar.
